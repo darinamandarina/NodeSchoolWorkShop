3a1fe531e4a93c90918ca79e04d1966eb2b5d2f2
darinamandarina <darinamandarina97@gmail.com>
2017-09-23 16:36:40 +0300

    completed 2st task

--- Es6WorkShop/program.js
+++ Es6WorkShop/program.js
@@ -1,2 +1,13 @@
-console.log(`Hello, ${process.argv[2]}!
-Your name lowercased is "${process.argv[2].toLowerCase()}".`);
+var inputs = process.argv.slice(2);
+var result = inputs.map(inputs=>inputs.split('')[0]).reverse().reduce((inputs,newString)=>newString.concat(inputs));
+
+console.log(`[${inputs}] becomes "${result}"`);
+
+/*
+* official solution
+*var inputs = process.argv.slice(2);
+*var result = inputs.map(s => s[0])
+* .reduce((soFar, s) => soFar + s);
+
+*console.log(`[${inputs}] becomes "${result}"`);
+ */
